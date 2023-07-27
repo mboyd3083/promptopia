47860cc202ce70deba045d58f34baa0040a05d95
Mike Boyd <mboyd3083@gmail.com>
2023-07-27 12:20:53 -0400

    modified nav bar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,19 +3,19 @@ import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
-import { set } from "mongoose";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const {data: session} = useSession();
+
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
       setProviders(response);
     };
-    setProviders();
+    setUpProviders();
   }, []);
 
   return (
@@ -30,18 +30,19 @@ const Nav = () => {
         />
         <p className="logo_text">Promptopia</p>
       </Link>
+
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
-              Create Prompt
+              Create Post
             </Link>
             <button type="button" onClick={signOut} className="outline_btn">
               Sign Out
             </button>
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user?.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -66,10 +67,10 @@ const Nav = () => {
         )}
       </div>
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user?.image}
               width={37}
               height={37}
               className="rounded-full"
@@ -82,7 +83,26 @@ const Nav = () => {
                   href="/profile"
                   className="dropdown_link"
                   onClick={() => setToggleDropdown((prev) => !prev)}
-                >My Profile</Link>
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown((prev) => !prev)}
+                >
+                  Create Post
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToggleDropdown((prev) => !prev);
+                    signOut();
+                  }}
+                  className="mt-5 w-full black_btn"
+                >
+                  Sign Out
+                </button>
               </div>
             )}
           </div>

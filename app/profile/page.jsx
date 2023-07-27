7a6236df6f96c6f30c02041f53c258f91b260251
Mike Boyd <mboyd3083@gmail.com>
2023-07-27 12:19:34 -0400

    created profile page

---
+++ app/profile/page.jsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+
+import { useRouter } from "next/navigation";
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+  console.log(session);
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+      setPosts(data);
+    };
+    if (session?.user.id) {
+      fetchPosts();
+    }
+  }, []);
+  return (
+    <>
+      {session ? (
+        <Profile
+          name="My"
+          desc="Welcome to your personalized profile page"
+          data={posts}
+          handleEdit={handleEdit}
+          handleDelete={handleDelete}
+        />
+      ) : (
+        <h1 className="head_text text-left">Sign in to view your profile</h1>
+      )}
+    </>
+  );
+};
+export default MyProfile;
